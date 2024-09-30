6cd3116dd3f12e2c229cbca3713e9b9b9d299f53
Tom Hardman <th624@cam.ac.uk>
2024-09-30 22:04:54 +0100

    3rd conv layer

--- RL/networks.py
+++ RL/networks.py
@@ -22,7 +22,7 @@ class BasicDQN(nn.Module):
 
 
 class BasicCNN(nn.Module):
-    def __init__(self, input_dim, input_channels, output_dim):
+    def __init__(self, input_dim, input_channels, output_dim, conv3=False):
         super().__init__()
         h = input_dim[0]
         w = input_dim[1]
@@ -30,12 +30,18 @@ class BasicCNN(nn.Module):
 
         self.conv1 = nn.Conv2d(input_channels, 16, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
+        if conv3:
+            self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
+        else:
+            self.conv3 = None
         self.fc1 = nn.Linear(flat, 256)
         self.fc2 = nn.Linear(256, output_dim)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        if self.conv3:
+            x = F.relu(self.conv3(x))  
         x = F.relu(self.fc1(x.flatten(-3)))
         x = self.fc2(x)
         return x
